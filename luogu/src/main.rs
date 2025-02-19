1130c0b2b7db194bf046fe076e39e3b1b37b2b93
E0SelmY4V <yabo1887415157@qq.com>
2025-02-19 20:26:09 +0800

    洛谷p5717

--- luogu/src/main.rs
+++ luogu/src/main.rs
@@ -5,7 +5,7 @@ macro_rules! use_as_now {
     };
 }
 
-use_as_now!(p1909);
+use_as_now!(p5717);
 
 pub fn main() {
     now::main();
---
+++ luogu/src/p5717.rs
@@ -0,0 +1,47 @@
+use ojan_luogu::*;
+use std::iter::Iterator;
+
+struct Triangle(u32, u32, u32);
+
+impl Triangle {
+    pub fn create(lines: impl Iterator<Item = u32>) -> Option<Triangle> {
+        let mut lines: Vec<_> = lines.take(3).collect();
+        lines.sort();
+        if lines[0] + lines[1] > lines[2] {
+            Some(Triangle(lines[0], lines[1], lines[2]))
+        } else {
+            None
+        }
+    }
+    pub fn print_shape(&self) {
+        let &Triangle(a, b, c) = self;
+        let cc = c * c;
+        let info = match a * a + b * b {
+            v if v > cc => "Acute triangle",
+            v if v < cc => "Obtuse triangle",
+            _ => "Right triangle",
+        };
+        println!("{}", info);
+    }
+    pub fn print_equ(&self) {
+        let &Triangle(a, b, c) = self;
+        let f1 = a == b;
+        let f2 = b == c;
+        if f1 || f2 {
+            println!("Isosceles triangle");
+            if f1 && f2 {
+                println!("Equilateral triangle");
+            }
+        }
+    }
+}
+
+pub fn main() {
+    let mut iner = input::new();
+    if let Some(triangle) = Triangle::create(iner.line().parse_to_iter()) {
+        triangle.print_shape();
+        triangle.print_equ();
+    } else {
+        println!("Not triangle");
+    }
+}
