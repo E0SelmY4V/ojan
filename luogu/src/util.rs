a8355932023ed93ab8f39c672e3237a8cb546c7c
E0SelmY4V <yabo1887415157@qq.com>
2025-02-19 18:07:41 +0800

    更正确的有效数字

--- luogu/src/util.rs
+++ luogu/src/util.rs
@@ -1,7 +1,32 @@
 pub fn format_significantly_f64(value: f64, figures: usize) -> String {
-    let digits = match value {
-        v if v <= 0.0 => figures,
-        _ => figures.saturating_sub(value.abs().log10() as usize + 1),
+    let digits = match value.abs() {
+        v if v >= 1. => figures.saturating_sub(value.abs().log10() as usize + 1),
+        v if v == 0. => figures - 1,
+        v  => (- v.log10().floor()) as usize - 1 + figures,
     };
     format!("{:.*}", digits, value)
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn format_significantly() {
+        assert_eq!("1.000", format_significantly_f64(1., 4));
+        assert_eq!("12.2300", format_significantly_f64(12.23, 6));
+        assert_eq!("123.000", format_significantly_f64(123., 6));
+        assert_eq!("12345", format_significantly_f64(12345., 4));
+        assert_eq!("0.1235", format_significantly_f64(0.123456, 4));
+        assert_eq!("0.1234", format_significantly_f64(0.123446, 4));
+        assert_eq!("0.00034500", format_significantly_f64(0.000345, 5));
+
+        assert_eq!("0.000", format_significantly_f64(0., 4));
+
+        assert_eq!("-1.000", format_significantly_f64(-1., 4));
+        assert_eq!("-12.2300", format_significantly_f64(-12.23, 6));
+        assert_eq!("-123.000", format_significantly_f64(-123., 6));
+        assert_eq!("-12345", format_significantly_f64(-12345., 4));
+        assert_eq!("-0.1235", format_significantly_f64(-0.123456, 4));
+        assert_eq!("-0.1234", format_significantly_f64(-0.123446, 4));
+        assert_eq!("-0.00034500", format_significantly_f64(-0.000345, 5));
+    }
 }
