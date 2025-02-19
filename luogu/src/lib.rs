02425e516985c823f8265c13fa8055968bef0d61
E0SelmY4V <yabo1887415157@qq.com>
2025-02-19 18:41:06 +0800

    洛谷p5715

--- luogu/src/lib.rs
+++ luogu/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod input;
 pub mod p1005;
 pub mod p5714;
+pub mod p5715;
 pub mod util;
---
+++ luogu/src/p5715.rs
@@ -0,0 +1,8 @@
+use crate::input;
+
+pub fn main() {
+    let mut iner = input::new();
+    let mut vec = iner.line().parse_to_iter().collect::<Vec<u8>>();
+    vec.sort();
+    vec.into_iter().for_each(|n| print!("{n} "));
+}
