356b69eb75082776e675d826fce86b3add5708fa
E0SelmY4V <yabo1887415157@qq.com>
2025-02-19 21:28:11 +0800

    洛谷p1424

---
+++ luogu/src/p1424.rs
@@ -0,0 +1,25 @@
+use ojan_luogu::*;
+
+pub fn main() {
+    let mut iner = input::new();
+    let mut line_iner = iner.line();
+    let x: u64 = line_iner.parse();
+    let n: u64 = line_iner.parse();
+    let sum: u64 = ((x - 1)..(x + n - 1)).map(|i| (i % 7 < 5) as u64).sum();
+    print!("{}", sum * 250);
+}
+
+/*
+
+0 1 2 3 4 5 6 7
+t t t t t f f t
+a_n = n % 7 < 5
+
+           d_n - (n%7<5)*250 = d_{n-1}
+d_n - 250\sum_{i=1}^n(i%7<5) = d_{n-1} - 250\sum_{i=1}^{n-1}(i%7<5) = d_1 - 250
+                         d_n = d_1 + 250\sum_{i=2}^n(i%7<5)
+
+  d_(x-1+n) - d_(x-1)
+=  250(\sum_{i=2}^(x-1+n)(i%7<5) - \sum_{i=2}^(x-1)(i%7<5))
+=  250\sum_{i=x}^(x-1+n)(i%7<5)
+*/
