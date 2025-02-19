99ad55dafd85ba664e69b8d669f5f75350225c82
E0SelmY4V <yabo1887415157@qq.com>
2025-02-19 18:32:04 +0800

    用宏来给浮点数类型分发有效数字方法

--- luogu/src/util.rs
+++ luogu/src/util.rs
@@ -1,32 +1,44 @@
-pub fn format_significantly_f64(value: f64, figures: usize) -> String {
-    let digits = match value.abs() {
-        v if v >= 1. => figures.saturating_sub(value.abs().log10() as usize + 1),
-        v if v == 0. => figures - 1,
-        v  => (- v.log10().floor()) as usize - 1 + figures,
-    };
-    format!("{:.*}", digits, value)
+pub trait SignificantlyFormatable {
+    fn format_significantly(&self, figures: usize) -> String;
 }
+macro_rules! impl_format_significantly {
+    ($type:ty) => {
+        impl SignificantlyFormatable for $type {
+            fn format_significantly(&self, figures: usize) -> String {
+                let digits = match self.abs() {
+                    v if v >= 1. => figures.saturating_sub(self.abs().log10() as usize + 1),
+                    v if v == 0. => figures - 1,
+                    v => (-v.log10().floor()) as usize - 1 + figures,
+                };
+                format!("{:.*}", digits, self)
+            }
+        }
+    };
+}
+impl_format_significantly!(f32);
+impl_format_significantly!(f64);
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn format_significantly() {
-        assert_eq!("1.000", format_significantly_f64(1., 4));
-        assert_eq!("12.2300", format_significantly_f64(12.23, 6));
-        assert_eq!("123.000", format_significantly_f64(123., 6));
-        assert_eq!("12345", format_significantly_f64(12345., 4));
-        assert_eq!("0.1235", format_significantly_f64(0.123456, 4));
-        assert_eq!("0.1234", format_significantly_f64(0.123446, 4));
-        assert_eq!("0.00034500", format_significantly_f64(0.000345, 5));
+        assert_eq!("1.000", 1_f64.format_significantly(4));
+        assert_eq!("12.2300", 12.23_f64.format_significantly(6));
+        assert_eq!("123.000", 123_f64.format_significantly(6));
+        assert_eq!("12345", 12345_f64.format_significantly(4));
+        assert_eq!("0.1235", 0.123456_f64.format_significantly(4));
+        assert_eq!("0.1234", 0.123446_f64.format_significantly(4));
+        assert_eq!("0.00034500", 0.000345_f64.format_significantly(5));
 
-        assert_eq!("0.000", format_significantly_f64(0., 4));
+        assert_eq!("0.000", 0_f64.format_significantly(4));
 
-        assert_eq!("-1.000", format_significantly_f64(-1., 4));
-        assert_eq!("-12.2300", format_significantly_f64(-12.23, 6));
-        assert_eq!("-123.000", format_significantly_f64(-123., 6));
-        assert_eq!("-12345", format_significantly_f64(-12345., 4));
-        assert_eq!("-0.1235", format_significantly_f64(-0.123456, 4));
-        assert_eq!("-0.1234", format_significantly_f64(-0.123446, 4));
-        assert_eq!("-0.00034500", format_significantly_f64(-0.000345, 5));
+        assert_eq!("-1.000", (-1_f64).format_significantly(4));
+        assert_eq!("-12.2300", (-12.23_f64).format_significantly(6));
+        assert_eq!("-123.000", (-123_f64).format_significantly(6));
+        assert_eq!("-12345", (-12345_f64).format_significantly(4));
+        assert_eq!("-0.1235", (-0.123456_f64).format_significantly(4));
+        assert_eq!("-0.1234", (-0.123446_f64).format_significantly(4));
+        assert_eq!("-0.00034500", (-0.000345_f64).format_significantly(5));
     }
 }
