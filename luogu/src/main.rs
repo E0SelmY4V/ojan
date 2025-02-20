9859ed35d194935ece9235c3330793661e5c9b98
E0SelmY4V <yabo1887415157@qq.com>
2025-02-20 13:08:49 +0800

    洛谷p1055 洛谷分支结构题单做完！撒花！

--- luogu/src/main.rs
+++ luogu/src/main.rs
@@ -5,7 +5,7 @@ macro_rules! use_as_now {
     };
 }
 
-use_as_now!(p4414);
+use_as_now!(p1055);
 
 pub fn main() {
     now::main();
---
+++ luogu/src/p1055.rs
@@ -0,0 +1,44 @@
+use ojan_luogu::*;
+
+fn digitify(digit_char: char) -> u16 {
+    match digit_char {
+        'X' => 10,
+        _ => digit_char as u16 - '0' as u16,
+    }
+}
+fn charify(digit: u16) -> char {
+    match digit {
+        10 => 'X',
+        _ => ('0' as u8 + digit as u8) as char,
+    }
+}
+
+pub fn main() {
+    let iner = input::new();
+    let mut code: u16 = 0;
+    let mut power: u16 = 0;
+    let isbn = iner.read_line();
+    let mut new_isbn = vec![];
+    let mut f = false;
+    for digit_char in isbn.chars().take(13) {
+        new_isbn.push(digit_char);
+        if digit_char == '-' {
+            continue;
+        }
+        let digit = digitify(digit_char);
+        if power == 9 {
+            f = digit == code;
+            break;
+        }
+        power += 1;
+        code = (code + (digit * power)) % 11;
+    }
+    new_isbn.pop();
+    let tip = if f {
+        String::from("Right")
+    } else {
+        new_isbn.push(charify(code));
+        String::from_iter(new_isbn)
+    };
+    print!("{tip}");
+}
