8b30c68901eaa5c66ecaf44c9a50b87108f7ff72
caufande <3823642480@qq.com>
2025-05-13 10:59:03 +0800

    简单化一些题

--- school/2-12.py
+++ school/2-12.py
@@ -2,33 +2,22 @@ import math
 
 a = float(input())
 b = float(input())
-c = float(input())
 
-def out(n):
-    print("%.2f"%n)
-
-def outi(a, b):
-    print("%.2f+%.2fi"%(a, b))
-    print("%.2f-%.2fi"%(a, b))
-
-def sign(n):
-    if n > 0:
-        return 1
-    elif n < 0:
-        return -1
+if a != 0:
+    a2 = a * 2
+    d = b ** 2 - 4 * a * float(input())
+    n = -b / a2
+    m = math.sqrt(abs(d)) / abs(a2)
+    if d >= 0:
+        print("%.2f"%(n + m))
+        print("%.2f"%(n - m))
     else:
-        return 0
-
-if a == 0:
+        print("%.2f+%.2fi"%(n, m))
+        print("%.2f-%.2fi"%(n, m))
+else:
     if b == 0:
         print("Not an equation")
-        exit()
-    out(-c / b)
-    exit()
-d = b * b - 4 * a * c
-if d >= 0:
-    out((-b + sign(a) * math.sqrt(d)) / (2 * a))
-    out((-b - sign(a) * math.sqrt(d)) / (2 * a))
-else:
-    outi(-b / (2 * a), abs(math.sqrt(abs(d)) / (2 * a)))
+    else:
+        print("%.2f"%(-float(input()) / b))
+
 
